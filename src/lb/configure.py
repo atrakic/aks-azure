17cfb75fceadc31e7090f11e3232f05c7ce4468d
Admir Trakic <Admir Trakic adtr@kp.dk>
2024-02-13 21:19:19 +0100

    Update lb

--- src/lb/configure.py
+++ src/lb/configure.py
@@ -7,31 +7,33 @@ def generate_nginx_config(hostnames: str = None):
     hosts = hostnames.split(",")
 
     config = "upstream backend {\n"
-    
+
     for host in hosts:
         config += f"    server {host.strip()};\n"
-    
+
     config += "}\n\n"
     config += "server {\n"
-    config += "    listen 80;\n"
-    config += "    server_name localhost;\n"
+    config += "    listen $SERVER_PORT;\n"
+    config += "    server_name $SERVER_NAME;\n"
     config += "\n"
     config += "    location / {\n"
     config += "        proxy_pass http://backend;\n"
     config += "        proxy_set_header X-Real-IP $remote_addr;\n"
+    config += "        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n"
+    config += "        proxy_set_header X-Forwarded-Proto $scheme;\n"
     config += "    }\n"
     config += "    location /status {\n"
     config += "       access_log off;\n"
     config += "       default_type text/plain;\n"
     config += "       expires -1;\n"
-    config += "       return 200 'Server address: $server_addr:$server_port\\nServer name: $hostname\\nDate: $time_local\\nURI: $request_uri\\nRequest ID: $request_id\\n';\n"
+    config += "       return 200 'Server address: $server_addr:$server_port\\nServer name: $server_name\\nDate: $time_local\\nURI: $request_uri\\nRequest ID: $request_id\\n';\n"
     config += "    }\n"
     config += "    location /health {\n"
     config += "         access_log off;\n"
     config += "         default_type application/json;\n"
-    config += '         return 200 \'{"status":"ok","version":"${APP_VERSION}"}\';\n'
+    config += '         return 200 \'{"status":"$status","version":"$APP_VERSION"}\';\n'
     config += "     }\n"
-    config += "    add_header X-Hello-Version ${APP_VERSION};\n"
+    config += "    add_header X-LB-Version ${APP_VERSION};\n"
     config += "}\n"
 
     return config
